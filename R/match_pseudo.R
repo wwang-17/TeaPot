f7ecf3c6d241b225f9ddb2f41379a96ceb53e86c
wangwei <wangwei@wangweideMacBook-Pro.local>
2022-07-11 16:12:46 -0400

    Initial

---
+++ R/match_pseudo.R
@@ -0,0 +1,14 @@
+
+
+#' Predict pseudotemporal information
+#'
+#' @param x cell data set (count matrix)
+#'
+#' @return predicted numeric pseudotime and branch
+#' @import Seurat
+#' @export
+#'
+#' @examples
+xs <- function(x){
+  x^3
+}
