9ba3f7ebbedb4c91e3218375d6dc51e90c7376e2
wangwei <wangwei@wangweideMacBook-Pro.local>
2022-07-11 22:29:48 -0400

    Edit of select.tissue function

--- R/select_tissue.R
+++ R/select_tissue.R
@@ -6,6 +6,15 @@
 #' @export
 #'
 #' @examples
-select_tissue <- function(input_matrix, standardize){
+select_tissue <- function(input_matrix, standardize, species){
+  #need to document this dataset
+
+  #import the pseudobulk of 56 reference trajectories
+  select.gene.count <- data("select.gene.count")
+
+  #normalization of the input matrix
+  m2.rawsum <- Matrix::rowSums(m2)
+  m2.normsum <- c(m2.rawsum) / sum(m2.rawsum) * 1e6
+  m2.normsum[which(m2.normsum != 0)] <- log2(m2.normsum[which(m2.normsum != 0)] + 1)
 
 }
